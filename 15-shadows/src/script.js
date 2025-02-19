faeb605926bcdd0ac2e59b2a084dd1d355f84885
Ravinder Singh Bisht <ravinderbisht5691@gmail.com>
2025-02-19 23:43:04 +0530

    add folder for different properties in threejs

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -18,18 +18,69 @@ const scene = new THREE.Scene()
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
 gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
+directionalLight.castShadow = true
+console.log(directionalLight.shadow)
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.width = 1024
+console.log(directionalLight.shadow.camera)
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 6
+// for control the blur of the shadow 
+directionalLight.shadow.radius = 10
+// follwing help in the much better shadow result
+// The smaller the values the more precise the shadow will be if it's too small the shadow will be cropped
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.left = -2
+// beacause this camera is a orthographic camera for we canmove it left right top and bottom.
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+directionalLightCameraHelper.visible = true
+
+scene.add(directionalLightCameraHelper)
+
+// spot light
+const spotLight = new THREE.SpotLight(0xffffff, 10, 5, Math.PI * 0.3)
+spotLight.castShadow = true
+spotLight.position.set(0, 2, 2)
+scene.add(spotLight)
+scene.add(spotLight.target)
+spotLight.shadow.camera.fov = 30
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
+
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+spotLightCameraHelper.visible = false
+scene.add(spotLightCameraHelper)
+
+//POint light
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.castShadow = true
+pointLight.position.set(-1, 1, 0)
+scene.add(pointLight)
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 6
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+pointLightCameraHelper.visible = false
+scene.add(pointLightCameraHelper)
+
 
 /**
  * Materials
@@ -46,6 +97,8 @@ const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
+sphere.position.y = 0.05
+sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
@@ -53,6 +106,8 @@ const plane = new THREE.Mesh(
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
+plane.receiveShadow = true
+
 
 scene.add(sphere, plane)
 
@@ -64,8 +119,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -102,13 +156,16 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap // the radius doesn't work pcfsoftshadow algo
+
+
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
